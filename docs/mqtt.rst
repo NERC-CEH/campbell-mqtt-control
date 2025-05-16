db2a7c01efdde47ce3a980a159e8cc279d9b9f02
Jo Walsh <jowals@ceh.ac.uk>
2025-05-16 16:46:43 +0100

    stub docs for testing MQTT connection

---
+++ docs/mqtt.rst
@@ -0,0 +1,21 @@
+
+
+MQTT 
+====
+
+General overview of MQTT interaction with the Campbell Logger including connection to the public test instance at Mosquitto
+
+Mosquitto test instance
+-----------------------
+
+https://test.mosquitto.org/
+
+mosquitto_sub
+
+Download the tarball
+
+sudo apt install cmake libssl-dev libcjson-dev
+make; sudo make install
+sudo /sbin/ldconfig
+
+https://mosquitto.org/man/mosquitto_sub-1.html
--- listener.py
+++ listener.py
@@ -1,9 +1,13 @@
+import logging
+
 from campbellcontrol.connection.generic import PahoConnection
 
+logging.basicConfig(level=logging.DEBUG)
+client_id = "QU8Q-9JTY-HVP8"
 conn = PahoConnection("test.mosquitto.org", 1883)
 conn.connect()
-conn.subscribe("cs/v2/+/QU8Q-9JTY-HVP8")
-conn.subscribe("cs/v2/+/QU8Q-9JTY-HVP8/#")
+conn.subscribe(f"cs/v2/+/{client_id}")
+conn.subscribe(f"cs/v2/+/{client_id}/#")
 conn.subscribe("cs/v2/#")
 # conn.subscribe("#")
 
