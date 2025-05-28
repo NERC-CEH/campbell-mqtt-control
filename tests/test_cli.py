2b2145a00cf08a44fc8a64728caf3f80da38ef17
Jo Walsh <metazool@fastmail.net>
2025-05-28 16:17:53 +0100

    ruff check for 4 spaces, seems fair

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 from click.testing import CliRunner
 from campbellcontrol.cli import cli
 
+
 def test_cli():
-  runner = CliRunner()
-  result = runner.invoke(cli)
-  assert result.exit_code == 2
-  assert "Show this message and exit" in result.output
+    runner = CliRunner()
+    result = runner.invoke(cli)
+    assert result.exit_code == 2
+    assert "Show this message and exit" in result.output
