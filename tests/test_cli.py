f74ee8803e4f670b4f29a6737379c4a075c65437
Jo Walsh <metazool@fastmail.net>
2025-05-28 16:08:30 +0100

    add the most minimal CLI test

---
+++ tests/test_cli.py
@@ -0,0 +1,8 @@
+from click.testing import CliRunner
+from campbellcontrol.cli import cli
+
+def test_cli():
+  runner = CliRunner()
+  result = runner.invoke(cli)
+  assert result.exit_code == 2
+  assert "Show this message and exit" in result.output
